08578ade714d2ef9ea26a6495e6939b5895da01b
notIsBot <thomazh2000@gmail.com>
2018-05-13 14:50:23 -0300

    13/08 - Android
    Application with the examples 5, 7 and 8.

---
+++ app578/app/src/main/java/a3aetim/app_578/Example08Activity.java
@@ -0,0 +1,13 @@
+package a3aetim.app_578;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Example08Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example08);
+    }
+}
